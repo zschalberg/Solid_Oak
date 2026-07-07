a8fb7c703d62725740ff57a74d080258ce296a2d
Zachary Schalberg <zschalberg@gmail.com>
2026-07-07 01:14:48 -0400

    feat: Declare TryAddFamilyReserveIconToHealthbox for healthbox indicator

--- include/battle_interface.h
+++ include/battle_interface.h
@@ -140,6 +140,7 @@ void CategoryIcons_LoadSpritesGfx(void);
 void TryToAddMoveInfoWindow(void);
 void TryToHideMoveInfoWindow(void);
 void TryAddPokeballIconToHealthbox(u8 healthboxSpriteId, bool8 noStatus);
+void TryAddFamilyReserveIconToHealthbox(u8 healthboxSpriteId, bool8 noStatus);
 void UpdateNickInHealthbox(u8 healthboxSpriteId, struct Pokemon *mon);
 
 #endif // GUARD_BATTLE_INTERFACE_H
